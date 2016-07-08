18903be6513438e5de3656f2304b2ed3361c6b50
Rahul Bishain <rahul.bishain@gmail.com>
2016-07-08 09:17:46 +0530

    graphs from 2 sheets

--- konkanPlot.py
+++ konkanPlot.py
@@ -5,13 +5,17 @@ import matplotlib.dates
 import csv, sys, re, random, os, datetime, time
 
 datfile="Konkan1.csv"
+datfile1="Konkan2.csv"
 Mcsv = csv.reader(open(datfile, 'rb'), delimiter=',', quotechar='"')
+Mcsv1 = csv.reader(open(datfile1, 'rb'), delimiter=',', quotechar='"')
 
 # Mapping from column name to column index
 I = {}
+I1 = {}
 
 # Matrix of all the rows
 M = []
+M1 = []
 
 rowNum = 0
 for row in Mcsv:
@@ -31,7 +35,26 @@ arrival_time_col = map(lambda r: datetime.datetime.strptime(r[I['Arrival Time']]
 dat = 'Distance'
 dat_col = map(lambda r: r[I[dat]], M)
 
-plt.plot(arrival_time_col, dat_col, label=dat, ls=lss[dat], color=colors[dat], marker=markers[dat], markersize=9, mew=2, linewidth=2)
+rowNum = 0
+for row in Mcsv1:
+    rowNum += 1
+    if (rowNum == 1):
+        for i in range(len(row)):
+            I1[row[i]] = i
+        continue
+    else:
+        M1.append(row)
+
+colors = { 'Distance':'blue'}
+markers = { 'Distance':'|'}
+lss = { 'Distance':'-'}
+
+arrival_time_col1 = map(lambda r: datetime.datetime.strptime(r[I1['Arrival Time']],"%d/%m/%y %H:%M"), M1)
+dat = 'Distance'
+dat_col1 = map(lambda r: r[I1[dat]], M1)
+#plt.figure(figsize=(20, 10))
+plt.plot(arrival_time_col, dat_col, label=dat, ls=lss[dat], color='blue', marker=markers[dat], markersize=9, mew=2, linewidth=2)
+plt.plot(arrival_time_col1, dat_col1, label=dat, ls=lss[dat], color='red', marker=markers[dat], markersize=9, mew=2, linewidth=2)
 plt.xlabel('Time of day (24hr)')
 plt.ylabel('Distance')
 plt.grid(b='on')
@@ -46,7 +69,6 @@ plt.grid(b='on')
 #axes.ticklabel_format(axis='both', style='plain')
 plt.legend()
 #plt.savefig("time_table.pdf", bbox_inches='tight')
-#plt.figure(figsize=(20, 2))
 plt.savefig("time_table.pdf")
 plt.show()
 #plt.plot_date(arrival_time_col,dat_col)
